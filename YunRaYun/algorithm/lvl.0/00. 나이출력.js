8550d60236e71ed9a9dde29869804b4eebd4a33c
YUNRAYUN <rayun.yun95@gmail.com>
2023-02-07 18:14:24 +0900

    알고리즘 lvl0

---
+++ YunRaYun/algorithm/lvl.0/00. 나이출력.js
@@ -0,0 +1,11 @@
+/* 
+문제 설명
+머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 
+나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+*/
+
+function solution(age) {
+  const curYear = 2022;
+  const bornYear = curYear - age + 1;
+  return bornYear;
+}

f01f2556f6c3def091efe896b87114add7727eda
abcd1234 <90763140+BackHong10@users.noreply.github.com>
2023-02-19 03:28:07 +0900

    오늘의 공부 알찼다.

--- HongSungJun/practice for node/index.js
+++ HongSungJun/practice for node/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import {router as goodsRouter} from './routes/good.js'
+import {cookieRouter} from './routes/cookie.js'
 const app = express()
 
 
 app.use("/api",[goodsRouter])
+
+app.use('/cookie', [cookieRouter])
 
 app.listen(3000, () => {
     console.log("3000번 포트로 서버가 열렸습니다.")
